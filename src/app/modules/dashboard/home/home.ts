7da5b728812fb9d0e40c873b71bc89a276d67db5
Luca <lucagiacalone76@gmail.com>
2025-07-04 22:57:27 +0200

    prende forma

--- src/app/modules/dashboard/home/home.ts
+++ src/app/modules/dashboard/home/home.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
+orders = [
+  { number: '57384710', date: '20/08/18', status: 'Shipped' },
+  { number: '93874653', date: '19/08/18', status: 'Open' },
+  { number: '98234967', date: '19/08/18', status: 'Open' },
+  { number: '93874653', date: '20/08/18', status: 'Shipped' },
+  { number: '98234967', date: '21/08/18', status: 'Open' }
+];
+
+deliveries = [
+  { number: '3309287', date: '16/08/18', status: 'Out for Delivery' },
+  { number: '3307285', date: '18/08/18', status: 'Out for Delivery' },
+  { number: '3307265', date: '19/08/18', status: 'Dispatched' },
+  { number: '3307259', date: '20/08/18', status: 'Dispatched' },
+  { number: '3307283', date: '21/08/18', status: 'Pending' }
+];
+
+payments = [
+  { number: '8276354', date: '10/08/18', amount: 236.52 },
+  { number: '0399376', date: '12/08/18', amount: 322.00 },
+  { number: '9207436', date: '19/08/18', amount: 534.12 },
+  { number: '3452757', date: '20/08/18', amount: 106.22 },
+  { number: '8376352', date: '21/08/18', amount: 218.11 }
+];
+
 
 }
--- src/app/modules/layout/layout.ts
+++ src/app/modules/layout/layout.ts
@@ -6,10 +6,35 @@ import { Router, RouterOutlet } from '@angular/router';
 import { Loader } from "../../shared/loader/loader/loader";
 import { AuthService } from '../../core/auth.service';
 
+import { Home } from '../dashboard/home/home';
+import { OrderList } from '../orders/order-list/order-list';
+import { OrderDetail } from '../orders/order-detail/order-detail';
+import { AddressList } from '../addresses/address-list/address-list';
+import { AddressForm } from '../addresses/address-form/address-form';
+import { StoreList } from '../stores/store-list/store-list';
+import { StoreForm } from '../stores/store-form/store-form';
+import { AccountSettings } from '../settings/account-settings/account-settings';
+import { Preferences } from '../settings/preferences/preferences';
+
 @Component({
   standalone: true,
   selector: 'app-layout',
-  imports: [CommonModule, Header, Sidebar, RouterOutlet, Loader],
+  imports: [
+        CommonModule,
+    RouterOutlet,
+    Header,
+    Sidebar,
+    Loader,
+    Home,
+    OrderList,
+    OrderDetail,
+    AddressList,
+    AddressForm,
+    StoreList,
+    StoreForm,
+    AccountSettings,
+    Preferences,
+  ],
   templateUrl: './layout.html',
   styleUrls: ['./layout.scss'],
 })
