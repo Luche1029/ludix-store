5920ccd2a2436870828e77ef3ad11ab7e59861e6
Luca <lucagiacalone76@gmail.com>
2025-07-16 23:50:01 +0200

    orders-details... finished

--- src/app/modules/orders/order-details/order-details.ts
+++ src/app/modules/orders/order-details/order-details.ts
@@ -4,18 +4,21 @@ import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { ApiService } from 'src/app/core/api.service';
 import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { environment } from 'src/app/core/constants';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-orders-detail',
   standalone: true,
   imports: [CommonModule, 
             CurrencyPipe, 
             DatePipe,
-            RouterLink
+            RouterLink,
+            FormsModule
           ],
   templateUrl: './order-details.html',
   styleUrl: './order-details.scss'
 })
 export class OrderDetails {
+
   constructor(
     private router: Router,
     private api: ApiService
@@ -33,8 +36,30 @@ export class OrderDetails {
   orderDetails: any = null;
   loading = true;
   error = '';
+  isEditable = false;
 
   ngOnInit(): void {
+    this.loadOrderDetails();
+    }
+
+    togglePcDetails() {
+      this.showPcComponents = !this.showPcComponents;
+    }
+
+    updateOrderStatus (status: number) {
+      this.api.post<any>('/setOrderStatus', { orderNumber: this.orderNumber, status}).subscribe({
+       next: res => {
+          this.loading = false;
+          this.loadOrderDetails();
+        },
+        error: err => {
+          console.error(err);
+          this.error = 'Errore durante il caricamento';
+          this.loading = false;
+        }
+      });
+    }
+  loadOrderDetails() {
     this.api.post<any>('/getOrderDetails', { orderNumber: this.orderNumber }).subscribe({
        next: res => {
           this.orderDetails = res.orderDetails;
@@ -46,15 +71,36 @@ export class OrderDetails {
           this.loading = false;
         }
       });
+  }
+
+    saveAddress(): void {
+      if (!this.orderDetails?.orderNumber) {
+        this.error = 'Order number missing';
+        return;
+      }
+
+      const body = {
+        orderNumber: this.orderDetails.orderNumber,
+        ...this.orderDetails.address
+      };
+
+      this.loading = true;
+      this.api.post<{ success: boolean }>('/saveOrderAddress', body)
+        .subscribe({
+          next: (res) => {
+            this.loading = false;
+            this.isEditable = false;
+            if (!res.success) {
+              this.error = 'Error saving address';
+            }
+          },
+          error: (err) => {
+            this.loading = false;
+            this.error = 'Server error';
+          }
+        });
     }
 
-    togglePcDetails() {
-      this.showPcComponents = !this.showPcComponents;
-    }
-
-    updateOrderStatus (statu: number) {
-      
-    }
 
     back() {
       this.router.navigate(['/orders']);
